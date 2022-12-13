06ccf1bdd172c864c4475932fe265245198fd90a
clouderMark <charnysheuski@yandex.by>
2022-12-13 11:17:21 +0300

    feat: add webpack config

---
+++ online-store/src/index.ts
@@ -0,0 +1,21 @@
+import './index.scss';
+
+class App {
+  x: number;
+
+  y: number;
+
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  calculate() {
+    return this.x * this.y;
+  }
+}
+
+const calculator = new App(50, 50);
+
+console.log(calculator.calculate());
+console.log('asdasdads');
